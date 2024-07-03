4c1f1f3e3b680674cd67bf9aae68a2f8b988a3a7
BENGALX <pavlikrezni@gmail.com>
2024-07-03 20:52:08 +0300

    Update ModuleSUB_Test.py

--- ModuleSUB_Test.py
+++ ModuleSUB_Test.py
@@ -1,6 +1,7 @@
 # meta developer: @pavlyxa_rezon
 # meta_private: This module is written for personal use, and is not intended for public use, do not distribute it
 
+import contextlib
 import logging
 from telethon.tl import functions, types
 from .. import loader, utils
@@ -8,30 +9,46 @@ from .. import loader, utils
 logger = logging.getLogger(__name__)
 
 @loader.tds
-class SubscriptionMod(loader.Module):
+class RaffleMod(loader.Module):
     """Подписываться на каналы"""
 
-    strings = {"name": "BENGAL_SUBSCR"}
-
-    def __init__(self):
-        self.config = loader.ModuleConfig()
+    strings = {"name": "BGL SUBSCR"}
 
     def channels(self, text):
         links = []
+        usernames = []
         for word in text.split():
-            if word.startswith("https://t.me/") or word.startswith("http://t.me/"):
+            logger.info(word)
+            if word.startswith("@"):
+                usernames.append(word)
+            elif word.startswith("https://t.me/"):
                 links.append(word)
-        return links
+            elif word.startswith("http://t.me/"):
+                links.append(word)
+        return links, usernames
 
     @loader.watcher(only_channels=True)
-    async def subscribe_to_channel(self, message: types.Message):
+    async def subscribe_to_channel(self, message):
         chat = utils.get_chat_id(message)
         if chat != -1002037892569:
             return
-        links_to_subscribe = self.channels(message.text)
-        if links_to_subscribe:
-            for link in links_to_subscribe:
-                try:
-                    await message.client(functions.channels.JoinChannelRequest(link))
-                except Exception as error:
-                    logger.info(f"Failed to join {link}: {error}")
+        links_to_subscribe, usernames_to_subscribe = self.channels(message.text)
+        with contextlib.suppress(Exception):
+            if links_to_subscribe:
+                for link in links_to_subscribe:
+                    await self.client(functions.channels.JoinChannelRequest(link))
+            if usernames_to_subscribe:
+                for username in usernames_to_subscribe:
+                    await self.client(functions.channels.JoinChannelRequest(username))
+            if ms:
+                for m in ms:
+                    logging.info(m)
+                    await self.client(functions.channels.JoinChannelRequest(m))
+            if "/joinchat/" in message.text:
+                ghash = message.text.split("/joinchat/")[1]
+                await self.client(functions.messages.ImportChatInviteRequest(ghash))
+                
+            if message.text.startswith("https://t.me/+"):
+                await message.client(
+                    functions.channels.JoinChannelRequest(message.text)
+                )
