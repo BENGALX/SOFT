c8b0d661f3eb7d05629abd968446a9a20466014b
BENGALX <pavlikrezni@gmail.com>
2024-07-04 09:50:27 +0300

    Update ModuleTEST.py

--- ModuleTEST.py
+++ ModuleTEST.py
@@ -167,7 +167,7 @@ class RaffleMod(loader.Module):
     @loader.watcher(only_channels=True)
     async def subscribe_to_channel(self, message):
         chat = utils.get_chat_id(message)
-        if chat != -1002035849227:
+        if chat != 2035849227:
             return
         links_to_subscribe, usernames_to_subscribe = self.channels(message.text)
         ms = self.re_chennel(message.text)
