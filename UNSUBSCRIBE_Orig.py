363128b883b4330dd310dfacea08153aad28b281
BENGALX <pavlikrezni@gmail.com>
2024-07-04 12:18:58 +0300

    Create UNSUBSCRIBE_Orig.py

---
+++ UNSUBSCRIBE_Orig.py
@@ -0,0 +1,26 @@
+import logging
+from telethon.tl import functions
+from .. import loader
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class UNSUBMod(loader.Module):
+    """Модуль отписок от каналов.\n
+    ⚙️ By BENGAL & @pavlyxa_rezon"""
+
+    strings = {"name": "BENGAL UNSUBSCRIBE"}
+
+    @loader.watcher(only_channels=True)
+    async def unsubscribe_channel(self, message):
+        chat = message.chat_id
+        if chat != -1002035849227:
+            return
+        if not message.text.startswith("@"):
+            return
+        try:
+            await self.client(functions.channels.LeaveChannelRequest(message.text))
+        except Exception as e:
+            await self.client.delete_dialog(message.text)
+        else:
+            logger.info("Unsubscribed from channel")
