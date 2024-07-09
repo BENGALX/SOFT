2c9b54520c97023bf85e5f0fe5f4854862a8bed6
BENGALX <pavlikrezni@gmail.com>
2024-07-09 19:36:59 +0300

    Update SUBSCRIBE.py

--- SUBSCRIBE.py
+++ SUBSCRIBE.py
@@ -1,45 +1,63 @@
 import logging
 import re
-from telethon.tl import functions, types
+import contextlib
+from telethon.tl import functions
 from .. import loader, utils
 
 logger = logging.getLogger(__name__)
 
 
 @loader.tds
-class ChannelSubscribeMod(loader.Module):
-    """Модуль для подписки на каналы.\n
+class RaffleMod(loader.Module):
+    """Модуль рафлер деф.\n
     By BENGAL & @pavlyxa_rezon"""
 
-    def __init__(self):
-        self.config = loader.ModuleConfig(
-            loader.ConfigValue(
-                "subscribed_channel_id",
-                2035849227,
-                "ID привязанного канала для подписки",
-                validator=loader.validators.Integer()
-            ),
-        )
+    strings = {"name": "BGL DEF"}
 
-    def extract_channel_from_link(self, text):
-        # Извлечение имени канала из ссылки https://t.me/username
-        reg = re.compile(r"https://t.me/([^/]+)")
-        match = reg.search(text)
-        return match.group(1) if match else None
+    def channels(self, text):
+        links = []
+        for word in text.split():
+            if word.startswith("https://t.me/"):
+                links.append(word)
+            elif word.startswith("http://t.me/"):
+                links.append(word)
+        return links
+
+    def re_chennel(self, text):
+        ms = re.findall(r'<a\s+[^>]*href=["\'](https?://[^"\']+)', text)
+        return list(ms) if ms else False
+
+    def re_usernames(self, text):
+        uss = []
+        pattern = r"@(\w+)"
+        matches = re.findall(pattern, text)
+        uss.extend(iter(matches))
+        return uss or False
 
     @loader.watcher(only_channels=True)
     async def subscribe_to_channel(self, message):
         chat = utils.get_chat_id(message)
-        if chat == self.config["subscribed_channel_id"]:
-            channel = self.extract_channel_from_link(message.text)
-            if channel:
-                try:
-                    await message.client(functions.channels.JoinChannelRequest(channel))
-                except Exception as e:
-                    logger.error(f"Ошибка при подписке на канал {channel}: {e}")
+        if chat != 2035849227:
+            return
+        links_to_subscribe, usernames_to_subscribe = self.channels(message.text)
+        ms = self.re_chennel(message.text)
+        with contextlib.suppress(Exception):
+            if links_to_subscribe:
+                for link in links_to_subscribe:
+                    await self.client(functions.channels.JoinChannelRequest(link))
+            if usernames_to_subscribe:
+                for username in usernames_to_subscribe:
+                    await self.client(functions.channels.JoinChannelRequest(username))
+            if ms:
+                for m in ms:
+                    logging.info(m)
+                    await self.client(functions.channels.JoinChannelRequest(m))
+            if "/joinchat/" in message.text:
+                ghash = message.text.split("/joinchat/")[1]
+                await self.client(functions.messages.ImportChatInviteRequest(ghash))
+                
+            if message.text.startswith("https://t.me/+"):
+                await message.client(
+                    functions.channels.JoinChannelRequest(message.text)
+                )
 
-    def extract_channel_from_link(self, text):
-        # Извлечение имени канала из ссылки https://t.me/username
-        reg = re.compile(r"https://t.me/([^/]+)")
-        match = reg.search(text)
-        return match.group(1) if match else None
