5afcb86f8da2f26338ebff483ae9646dac0f4b0a
BENGALX <pavlikrezni@gmail.com>
2024-07-09 19:15:28 +0300

    Update UNSUBSCRIBE.py

--- UNSUBSCRIBE.py
+++ UNSUBSCRIBE.py
@@ -12,7 +12,7 @@ class UNSUBMod(loader.Module):
     strings = {"name": "BGL_UNSUBSCR"}
 
     async def send_unsubscribe_message(self, chat_id, channel_name):
-        text = f"Вы успешно отписались от {channel_name}"
+        text = f"<b>Вы успешно отписались от {channel_name}</b>"
         await self.inline.bot.send_message(chat_id, text=text, parse_mode="html")
 
     @loader.watcher(only_channels=True)
@@ -27,6 +27,6 @@ class UNSUBMod(loader.Module):
             await self.send_unsubscribe_message(self.tg_id, message.text)
         except Exception as e:
             await self.client.delete_dialog(message.text)
-            logger.error(f"Failed to unsubscribe from channel: {e}")
+            logger.error(f"Failed to unsubscribe from channel. {e}")
         else:
-            logger.info("Unsubscribed from channel")
+            logger.info("Unsubscribed from channel.")
