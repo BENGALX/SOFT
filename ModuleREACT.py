85dd600e2ad04b23fc2b5f0b6bbbf499edc31d1e
BENGALX <pavlikrezni@gmail.com>
2024-07-09 17:08:00 +0300

    Update ModuleREACT.py

--- ModuleREACT.py
+++ ModuleREACT.py
@@ -1,23 +1,33 @@
-# meta developer: @your_username
-# meta_private: This module is written for personal use, and is not intended for public use, do not distribute it
+import random
+import asyncio
+from telethon.tl import functions
 
-from .. import loader
-
-import logging
-
-logger = logging.getLogger(__name__)
-
+from .. import loader, utils
 
 @loader.tds
-class TestLogMod(loader.Module):
-    """Простой модуль для проверки логов"""
-
-    strings = {"name": "TestLog"}
+class ReactionModule(loader.Module):
+    """Модуль для ставки рандомных положительных реакций на посты в канале"""
 
     def __init__(self):
-        self.config = loader.ModuleConfig()
+        self.config = loader.ModuleConfig(
+            "target_channel_id",
+            [],
+            "Айди канала, на который будут ставиться реакции",
+            lambda x: list(map(int, x.split())),
+        )
 
-    async def client_ready(self, client, db):
-        self.client = client
-        logger.info('TestLogMod is ready and logging works')
-        print("TestLogMod is ready and logging works (print statement)")
+    @loader.watcher(only_incoming=True, pattern=r".*")
+    async def watch_and_react(self, message):
+        chat_id = message.chat_id
+        if chat_id not in self.config["target_channel_id"]:
+            return
+
+        try:
+            await message.client(functions.messages.AddReactionRequest(
+                message.chat_id,
+                message.id,
+                random.choice(['👍', '❤️', '😊'])
+            ))
+        except Exception as e:
+            print(f"Ошибка при добавлении реакции: {e}")
+
