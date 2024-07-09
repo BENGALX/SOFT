fb85736178bf7002acfb233b3d66cbfa890ecd0c
BENGALX <pavlikrezni@gmail.com>
2024-07-09 19:38:43 +0300

    Update SUBSCRIBE.py

--- SUBSCRIBE.py
+++ SUBSCRIBE.py
@@ -1,7 +1,8 @@
-import logging
-import re
 import contextlib
-from telethon.tl import functions
+import logging
+import asyncio
+import re
+from telethon.tl import functions, types
 from .. import loader, utils
 
 logger = logging.getLogger(__name__)
@@ -14,14 +15,37 @@ class RaffleMod(loader.Module):
 
     strings = {"name": "BGL DEF"}
 
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "channel_id",
+                [],
+                lambda: "Айди каналов, в которых будет участвовать модуль",
+                validator=loader.validators.Series(
+                    loader.validators.Union(
+                        loader.validators.Integer(),
+                    )
+                ),
+            ),
+        )
+
     def channels(self, text):
         links = []
+        usernames = []
         for word in text.split():
-            if word.startswith("https://t.me/"):
+            logger.info(word)
+            if word.startswith("@"):
+                usernames.append(word)
+            elif word.startswith("https://t.me/"):
                 links.append(word)
             elif word.startswith("http://t.me/"):
                 links.append(word)
-        return links
+        return links, usernames
+
+    def extract_channel_and_post_id(self, text):
+        # https://t.me/username/123 -> username, 123
+        reg = re.compile(r"https://t.me/([^/]+)/(\d+)")
+        return (match[1], match[2]) if (match := reg.search(text)) else False
 
     def re_chennel(self, text):
         ms = re.findall(r'<a\s+[^>]*href=["\'](https?://[^"\']+)', text)
@@ -33,6 +57,110 @@ class RaffleMod(loader.Module):
         matches = re.findall(pattern, text)
         uss.extend(iter(matches))
         return uss or False
+
+    @loader.watcher(only_channels=True)
+    async def post_checker(self, message: types.Message):
+        chat = utils.get_chat_id(message)
+        if chat in self.config["channel_id"]:
+            text = message.message
+            if message.reply_markup:
+                links_to_subscribe, usernames_to_subscribe = self.channels(text)
+                logger.info(links_to_subscribe)
+                logger.info(usernames_to_subscribe)
+                uus = self.re_usernames(text)
+                ms = self.re_chennel(text)
+                logging.info(ms)
+                try:
+                    if links_to_subscribe:
+                        for link in links_to_subscribe:
+                            await message.client(
+                                functions.channels.JoinChannelRequest(link)
+                            )
+                    if usernames_to_subscribe:
+                        for username in usernames_to_subscribe:
+                            await message.client(
+                                functions.channels.JoinChannelRequest(username)
+                            )
+                    if ms:
+                        for m in ms:
+                            logging.info(m)
+                            await message.client(
+                                functions.channels.JoinChannelRequest(m)
+                            )
+                    if uus:
+                        for u in uus:
+                            logging.info(u)
+                            await message.client(
+                                functions.channels.JoinChannelRequest(u)
+                            )
+                except Exception as error:
+                    logging.info(error)
+                    
+                await asyncio.sleep(5)
+                await message.click(0)
+                ent = await self.client.get_entity(message.chat_id)
+                text = (
+                    f"🎉 <b>Вы успешно участвуете в розыгрыше! в канале:</b> {ent.title}\n"
+                    f"💬 <b>Розыгрыш</b>: https://t.me/{ent.username}/{message.id}\n"
+                )
+                await self.inline.bot.send_message(
+                    self.tg_id, text=text, parse_mode="html"
+                )
+
+    @loader.watcher(only_channels=True)
+    async def get_and_subscribe(self, message: types.Message):
+        """
+        This watcher will take the link to post and get the message and do the raffle
+        """
+        chat = message.chat_id
+        if chat != -1002035849227:
+            return
+        em = message.text
+        logger.info(em)
+        channel, _id = self.extract_channel_and_post_id(em)
+        uss = self.re_usernames(em)
+        if not channel or not _id:
+            return logger.info("No channel or post id")
+        logger.info(channel)
+        logger.info(_id)
+        post = (await self.client.get_messages(channel, ids=[int(_id)]))[0]
+        if post.reply_markup:
+            links_to_subscribe, usernames_to_subscribe = self.channels(post.text)
+            logger.info(links_to_subscribe)
+            logger.info(usernames_to_subscribe)
+            ms = self.re_chennel(post.text)
+            try:
+                if links_to_subscribe:
+                    for link in links_to_subscribe:
+                        await self.client(functions.channels.JoinChannelRequest(link))
+                if usernames_to_subscribe:
+                    for username in usernames_to_subscribe:
+                        await self.client(
+                            functions.channels.JoinChannelRequest(username)
+                        )
+                if ms:
+                    for m in ms:
+                        logging.info(m)
+                        await self.client(functions.channels.JoinChannelRequest(m))
+                if uss:
+                    for u in uss:
+                        logging.info(u)
+                        await self.client(functions.channels.JoinChannelRequest(u))
+            except Exception as e:
+                logger.info(e)
+            logger.info("clicking")
+        huy = await post.click(0)
+        logger.info(huy)
+        await asyncio.sleep(5)
+        ent = await self.client.get_entity(post.chat_id)
+        text = (
+            f"🎉 <b>Вы успешно участвуете в розыгрыше! в канале:</b> {ent.title}\n"
+            f"💬 <b>Розыгрыш</b>: https://t.me/{ent.username}/{post.id}\n"
+        )
+        S = await post.click(0)
+        await post.click(0)
+        logger.info(S)
+        await self.inline.bot.send_message(self.tg_id, text=text, parse_mode="html")
 
     @loader.watcher(only_channels=True)
     async def subscribe_to_channel(self, message):
@@ -60,4 +188,3 @@ class RaffleMod(loader.Module):
                 await message.client(
                     functions.channels.JoinChannelRequest(message.text)
                 )
-
