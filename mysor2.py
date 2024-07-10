32ca9c14836eb8b84114c9828c01feda101d2793
BENGALX <pavlikrezni@gmail.com>
2024-07-10 10:07:22 +0300

    Update mysor2.py

--- mysor2.py
+++ mysor2.py
@@ -1,5 +1,3 @@
-# meta developer: @SNEGIREKgg
-
 import asyncio
 import logging
 import re
@@ -10,20 +8,23 @@ from telethon.tl.functions.messages import ImportChatInviteRequest
 logger = logging.getLogger(__name__)
 
 @loader.tds
-class SubVMod(loader.Module):
-    """Модуль"""
+class SUBMod(loader.Module):
+    """Модуль подписок на каналы.\n
+    By BENGAL & @pavlyxa_rezon"""
 
-    strings = {"name": "SubV"}
+    strings = {"name": "BGL_SUBSCR"}
 
     def __init__(self):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
-                "chat_id",
-                2035849227,
-                "ID",
+                "chat_id", 2035849227, "ID",
                 validator=loader.validators.Integer(),
             )
         )
+
+    async def send_subscribe_message(self, chat_id, channel_name):
+        text = f"<b>Вы успешно подписались на {channel_name}</b>"
+        await self.inline.bot.send_message(chat_id, text=text, parse_mode="html")
 
     @loader.watcher()
     async def watcher(self, message):
@@ -34,8 +35,10 @@ class SubVMod(loader.Module):
                     for link in links:
                         try:
                             await self._client(JoinChannelRequest(channel=link))
+                            await self.send_subscribe_message(self.config["chat_id"], link)
                         except:
                             await self._client(ImportChatInviteRequest(link.split("t.me/+")[1]))
+                            await self.send_subscribe_message(self.config["chat_id"], link)
         except:
             pass
         try:
@@ -45,7 +48,9 @@ class SubVMod(loader.Module):
                     for link in links:
                         try:
                             await self._client(JoinChannelRequest(channel=link))
+                            await self.send_subscribe_message(self.config["chat_id"], link)
                         except:
                             await self._client(ImportChatInviteRequest(link.split("t.me/+")[1]))
+                            await self.send_subscribe_message(self.config["chat_id"], link)
         except:
             pass
