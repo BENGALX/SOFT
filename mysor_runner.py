c917f16df7718aaf4c20071bcd75889e99fbaefb
BENGALX <pavlikrezni@gmail.com>
2024-07-10 20:27:53 +0300

    Update and rename mysor3.py to mysor_runner.py

---
+++ mysor_runner.py
@@ -0,0 +1,67 @@
+import asyncio
+import logging
+import re
+from .. import loader, utils
+from telethon.tl.types import PeerChannel
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class RunButtonMod(loader.Module):
+    """Модуль нажатия деф кнопок.\n
+    By BENGAL & @pavlyxa_rezon"""
+
+    strings = {"name": "BGL_RUNNER_DEF"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "chat_id", 2035849227, "ID",
+                validator=loader.validators.Integer(),
+            )
+        )
+
+    @loader.watcher()
+    async def watcher(self, message):
+        try:
+            if message.peer_id.channel_id == self.config["chat_id"]:
+                if "t.me/" in message.message:
+                    links = re.findall(r'https?://t.me/c/.*/.*', message.message)
+                    links1 = re.findall(r'https?://t.me/.*/.*', message.message)
+                    answer = ""
+                    for link in links:
+                        link = link.split("//t.me/c/")[1]
+                        link = link.split("/")
+                        b_msg = await self._client.get_messages(PeerChannel(int(link[0])), ids=int(link[1]))
+                        click = await b_msg.click(data=b_msg.reply_markup.rows[0].buttons[0].data)
+                        answer = answer + click.message + "\n"
+                    for link in links1:
+                        link = link.split("//t.me/")[1]
+                        link = link.split("/")
+                        b_msg = await self._client.get_messages(link[0], ids=int(link[1]))
+                        click = await b_msg.click(data=b_msg.reply_markup.rows[0].buttons[0].data)
+                        answer = answer + click.message + "\n"
+                    await utils.answer(message, answer)
+        except:
+            pass
+        try:
+            if message.peer_id.chat_id == self.config["chat_id"]:
+                if "t.me/" in message.message:
+                    links = re.findall(r'https?://t.me/c/.*/.*', message.message)
+                    links1 = re.findall(r'https?://t.me/.*/.*', message.message)
+                    answer = ""
+                    for link in links:
+                        link = link.split("//t.me/c/")[1]
+                        link = link.split("/")
+                        b_msg = await self._client.get_messages(PeerChannel(int(link[0])), ids=int(link[1]))
+                        click = await b_msg.click(data=b_msg.reply_markup.rows[0].buttons[0].data)
+                        answer = answer + click.message + "\n"
+                    for link in links1:
+                        link = link.split("//t.me/")[1]
+                        link = link.split("/")
+                        b_msg = await self._client.get_messages(link[0], ids=int(link[1]))
+                        click = await b_msg.click(data=b_msg.reply_markup.rows[0].buttons[0].data)
+                        answer = answer + click.message + "\n"
+                    await utils.answer(message, answer)
+        except:
+            pass
