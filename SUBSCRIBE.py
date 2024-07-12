ac7ba3aef826f855a31fe79e73264567b7beb0f4
BENGALX <pavlikrezni@gmail.com>
2024-07-12 12:25:26 +0300

    Update SUBSCRIBE.py

--- SUBSCRIBE.py
+++ SUBSCRIBE.py
@@ -2,6 +2,7 @@ import logging
 import re
 from telethon.tl.functions.channels import JoinChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.tl.types import PeerChannel, PeerChat
 from .. import loader
 
 logger = logging.getLogger(__name__)
@@ -13,28 +14,28 @@ class SUBMod(loader.Module):
 
     strings = {"name": "BGL_SUBSCR"}
     
+    async def send_bot_message(self, text):
+        await self.client.send_message('me', text)
+
     @loader.watcher()
     async def watcher(self, message):
         chat_id = 2035849227
         try:
-            if message.peer_id.channel_id == chat_id:
+            if isinstance(message.peer_id, (PeerChannel, PeerChat)) and message.peer_id.channel_id == chat_id:
                 if "t.me/" in message.message:
                     links = re.findall(r'https?://t.me/.*', message.message)
                     for link in links:
+                        success_message = f"<b>Вы успешно подписались на</b> {link}"
                         try:
                             await self.client(JoinChannelRequest(channel=link))
-                        except:
-                            await self.client(ImportChatInviteRequest(link.split("t.me/+")[1]))
-        except:
-            pass
-        try:
-            if message.peer_id.chat_id == chat_id:
-                if "t.me/" in message.message:
-                    links = re.findall(r'https?://t.me/.*', message.message)
-                    for link in links:
-                        try:
-                            await self.client(JoinChannelRequest(channel=link))
-                        except:
-                            await self.client(ImportChatInviteRequest(link.split("t.me/+")[1]))
-        except:
-            pass
+                            await self.send_bot_message(success_message)
+                        except Exception as e:
+                            logger.error(f"Error joining channel: {e}")
+                            try:
+                                invite_hash = link.split("t.me/+")[1]
+                                await self.client(ImportChatInviteRequest(invite_hash))
+                                await self.send_bot_message(success_message)
+                            except Exception as e:
+                                logger.error(f"Error importing chat invite: {e}")
+        except Exception as e:
+            logger.error(f"Error in watcher: {e}")
