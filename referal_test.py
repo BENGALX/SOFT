23b8e0e17bd143d6c9185697b66c14c4fab74e04
BENGALX <pavlikrezni@gmail.com>
2024-07-12 14:06:59 +0300

    Update referal_test.py

--- referal_test.py
+++ referal_test.py
@@ -13,47 +13,37 @@ class RefkaMod(loader.Module):
 
     strings = {"name": "BGL_REFKA"}
 
-    async def start_the_fastest_bot(self, param):
-        await self._client(
-            StartBotRequest(
-                bot="TheFastesRuBot", peer="TheFastesRuBot", start_param=param
-            )
-        )
-        logging.info(f"Bot started ref: {param} for TheFastesRuBot")
-
-    async def start_best_random_bot(self, param):
-        await self._client(
-            StartBotRequest(
-                bot="BestRandom_bot", peer="BestRandom_bot", start_param=param
-            )
-        )
-        logging.info(f"Bot started ref: {param} for BestRandom_bot")
-
-    async def start_default_bot(self, param):
-        await self._client(
-            StartBotRequest(
-                bot="TheFastes_Bot", peer="TheFastes_Bot", start_param=param
-            )
-        )
-        logging.info(f"Bot started ref: {param} for default bot")
-
     async def attempt_to_start(self, text):
         if match := re.search(r"\?start=(\w+)", text):
-            param = match[1]
-
-            logging.info(f"Bot started ref: {param}")
-            logging.info("TEXT: " + text)
+            key = match[1]
+            logging.info(f"Bot started ref: {key}")
 
             if "TheFastesRuBot" in text:
-                await self.start_the_fastest_bot(param)
-            elif "BestRandom_bot" in text:
-                await self.start_best_random_bot(param)
-            else:
-                await self.start_default_bot(param)
+                await self._client(
+                    StartBotRequest(
+                        bot="TheFastesRuBot", peer="TheFastesRuBot", start_ref=key
+                    )
+                )
+                return
+
+            if "BestRandom_bot" in text:
+                await self._client(
+                    StartBotRequest(
+                        bot="BestRandom_bot", peer="BestRandom_bot", start_ref=key
+                    )
+                )
+                return
+
+             if "TheFastes_Bot" in text:
+                await self._client(
+                    StartBotRequest(
+                        bot="TheFastes_Bot", peer="TheFastes_Bot", start_ref=key
+                    )
+                )
+                return
 
     @loader.watcher(only_channels=True)
     async def watcher_bot(self, message: Message):
         if message.chat_id != -1002219293691:
             return
-
         await self.attempt_to_start(message.text)
