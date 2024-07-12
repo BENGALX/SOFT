aa40204b8b301897a5dc3a407131918e23efff79
BENGALX <pavlikrezni@gmail.com>
2024-07-12 13:51:29 +0300

    Update referalkatest.py

--- referalkatest.py
+++ referalkatest.py
@@ -13,13 +13,12 @@ class RefkaMod(loader.Module):
 
     strings = {"name": "BGL_REFKA"}
 
-    async def start_the_fastest_bot(self, key):
+    async def start_the_fastes_bot(self, key):
         await self._client(
             StartBotRequest(
                 bot="TheFastesRuBot", peer="TheFastesRuBot", start_ref=key
             )
         )
-        logging.info(f"Bot started ref: {key} for TheFastesRuBot")
 
     async def start_best_random_bot(self, key):
         await self._client(
@@ -27,33 +26,28 @@ class RefkaMod(loader.Module):
                 bot="BestRandom_bot", peer="BestRandom_bot", start_ref=key
             )
         )
-        logging.info(f"Bot started ref: {key} for BestRandom_bot")
 
-    async def start_default_bot(self, key):
+    async def start_the_fastes_bot(self, key):
         await self._client(
             StartBotRequest(
                 bot="TheFastes_Bot", peer="TheFastes_Bot", start_ref=key
             )
         )
-        logging.info(f"Bot started ref: {key} for default bot")
 
     async def attempt_to_start(self, text):
         if match := re.search(r"\?start=(\w+)", text):
             key = match[1]
-
             logging.info(f"Bot started ref: {key}")
-            logging.info("TEXT: " + text)
 
             if "TheFastesRuBot" in text:
                 await self.start_the_fastest_bot(key)
-            elif "BestRandom_bot" in text:
+            if "BestRandom_bot" in text:
                 await self.start_best_random_bot(key)
-            else:
-                await self.start_default_bot(key)
+            if "BestRandom_bot" in text:
+                await self.start_the_fastes_bot(key)
 
     @loader.watcher(only_channels=True)
     async def watcher_bot(self, message: Message):
         if message.chat_id != -1002219293691:
             return
-
         await self.attempt_to_start(message.text)
