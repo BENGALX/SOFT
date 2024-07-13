142cbf7ab4d40ab5d68f967491fe4748148b10ea
BENGALX <pavlikrezni@gmail.com>
2024-07-13 20:34:24 +0300

    Update and rename mysor_proba.py to _probarefki.py

---
+++ _probarefki.py
@@ -0,0 +1,53 @@
+import logging
+import re
+from telethon.tl.types import Message
+from telethon.tl.functions.messages import StartBotRequest
+from .. import loader
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class RefkaMod(loader.Module):
+    """Модуль участия в рефках.\n
+    By BENGAL & @pavlyxa_rezon"""
+
+    strings = {"name": "BGL_REFKA"}
+
+    async def start_fastesru_bot(self, ref_key):
+        await self._client(
+            StartBotRequest(
+                bot="TheFastesRuBot", peer="TheFastesRuBot", start_param=ref_key
+            )
+        )
+
+    async def start_bestrandom_bot(self, ref_key):
+        await self._client(
+            StartBotRequest(
+                bot="BestRandom_bot", peer="BestRandom_bot", start_param=ref_key
+            )
+        )
+
+    async def start_fastes_bot(self, ref_key):
+        await self._client(
+            StartBotRequest(
+                bot="TheFastes_Bot", peer="TheFastes_Bot", start_param=ref_key
+            )
+        )
+
+    async def attempt_to_start(self, text):
+        if match := re.search(r"\?start=(\w+)", text):
+            ref_key = match[1]
+            logging.info(f"Bot started ref: {ref_key}")
+
+            if "TheFastesRuBot" in text:
+                await self.start_fastesru_bot(ref_key)
+            if "BestRandom_bot" in text:
+                await self.start_bestrandom_bot(ref_key)
+            if "TheFastes_Bot" in text:
+                await self.start_fastes_bot(ref_key)
+
+    @loader.watcher(only_channels=True)
+    async def watcher_bot(self, message: Message):
+        if message.chat_id != -1002219293691:
+            return
+        await self.attempt_to_start(message.text)
