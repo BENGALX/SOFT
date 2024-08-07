b7965ada6cc802fb752d17b9d4b80d0106936b3c
BENGALX <pavlikrezni@gmail.com>
2024-08-07 20:12:39 +0300

    Update and rename text_test.py to sms_test.py

---
+++ sms_test.py
@@ -0,0 +1,32 @@
+import logging
+from telethon.tl.functions.messages import SendMessageRequest
+from .. import loader
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class MessageSenderMod(loader.Module):
+    """Модуль для отправки SMS в чат.\n
+    By BENGAL & @pavlyxa_rezon"""
+
+    strings = {"name": "BGL_MESSAGE_SENDER"}
+
+    async def send_message(self, chat_username, message_text):
+        try:
+            chat_entity = await self._client.get_entity(chat_username)
+            await self._client(SendMessageRequest(peer=chat_entity, message=message_text))
+            logger.info(f"Message sent to {chat_username}: {message_text}")
+        except Exception as e:
+            logger.error(f"Error sending message to {chat_username}: {e}")
+
+    @loader.watcher()
+    async def watcher(self, message):
+        chat_id = -1002035849227  # Айди чата, куда нужно отправлять сообщения
+        if message.chat_id == chat_id and message.text.startswith("/sms"):
+            args = message.text.split(" ", 2)
+            if len(args) < 3:
+                await message.respond("Usage: /sms <chat_username> <message_text>")
+                return
+            chat_username, message_text = args[1], args[2]
+            await self.send_message(chat_username, message_text)
+            await message.respond(f"Message sent to {chat_username}: {message_text}")
