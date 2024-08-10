ddaad936c123a9fed07426b5daecb03085d1c23c
BENGALX <pavlikrezni@gmail.com>
2024-08-10 23:46:09 +0300

    Update and rename SUBSCRIBE,.py to SUBSCRIBE.py

--- SUBSCRIBE.py
+++ SUBSCRIBE.py
@@ -6,7 +6,7 @@ from .. import loader
 @loader.tds
 class SUBMod(loader.Module):
     """Модуль подписок на каналы. \n
-    Command: /sub .\n
+    Command: /sub.\n
     By BENGAL & @pavlyxa_rezon\n"""
 
     strings = {"name": "BGL-SUBSCRIBE"}
