44f368308b1e12eda686099e20f41630ba41968d
BENGALX <pavlikrezni@gmail.com>
2024-08-10 18:02:35 +0300

    Update and rename STEALER.py to CRYPTOSTEALER.py

--- CRYPTOSTEALER.py
+++ CRYPTOSTEALER.py
@@ -9,7 +9,7 @@ class StealerMod(loader.Module):
     """Модуль для ловли чеков.\n
     By BENGAL & @pavlyxa_rezon"""
 
-    strings = {"name": "BGL_STEALER"}
+    strings = {"name": "BGL-CRYPTOSTEALER"}
 
     async def mess(self, text: str, user_id: int):
         async with self.client.conversation(user_id) as conv:
@@ -31,7 +31,6 @@ class StealerMod(loader.Module):
                 links = re.findall(r'https?://t.me/.*', message.message)
                 done_message = f"<b>Вы успешно снюхали чек.</b> \n {links}"
                 fail_message = f"<b>Не удалось активировать чек.</b> \n {links}"
-                
                 for link in links:
                     u = link.split('?start=')
                     ref_code = u[1]
@@ -41,5 +40,16 @@ class StealerMod(loader.Module):
                         await self.send_bot_message(done_message)
                     except:
                         await self.send_bot_message(fail_message)
+            
+            if "/pass" in message.message:
+                ref_code = message.message.split("/pass", 1)[-1].strip()
+                done_message = f"<b>Успешно введен пароль:</b> {ref_code}"
+                fail_message = f"<b>Не удалось ввести пароль:</b> {ref_code}"
+                cbot = "@CryptoBot"
+                try:
+                    await self.mess(f'{ref_code}', cbot)
+                    await self.send_bot_message(done_message)
+                except:
+                    await self.send_bot_message(fail_message)
         except:
             pass
