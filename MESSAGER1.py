7afa291540e3b2498a8166c9eb1e9dede24b249e
BENGALX <pavlikrezni@gmail.com>
2024-08-12 17:03:06 +0300

    Update and rename MESSAGER.py to MESSAGER1.py

--- MESSAGER1.py
+++ MESSAGER1.py
@@ -1,23 +1,27 @@
-import logging
 from telethon.tl.functions.messages import SendMessageRequest
 from .. import loader
 
-logger = logging.getLogger(__name__)
-
 @loader.tds
 class MessagerMod(loader.Module):
-    """Модуль для отправки SMS в чат.\n
-    By BENGAL & @pavlyxa_rezon"""
+    """Модуль для рассылки SMS в чат.
+           Commands: /sms.\n
+    ⚙️ By BENGAL & @pavlyxa_rezon"""
 
-    strings = {"name": "BGL_MESSAGER"}
+    strings = {"name": "BGL-MESSAGER"}
 
     async def send_message(self, chat_username, message_text):
+        done_message = f"<b>В чат {chat_username} успешно отправлена рассылка:</b>\n {message_text}"
+        fail_message = f"<b>Не удалось отправить в {chat_username} сообщение:</b>\n {message_text}"
+        
         try:
             chat_entity = await self._client.get_entity(chat_username)
             await self._client(SendMessageRequest(peer=chat_entity, message=message_text))
-            logger.info(f"Message sent to {chat_username}: {message_text}")
-        except Exception as e:
-            logger.error(f"Error sending message to {chat_username}: {e}")
+            await self.send_me_message(done_message)
+        except:
+            await self.send_me_message(fail_message)
+
+    async def send_me_message(self, text):
+        await self.client.send_message('me', text)
 
     @loader.watcher()
     async def watcher(self, message):
