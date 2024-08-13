bf066e2064a5f0c91cf28765837849acea11a0aa
BENGALX <pavlikrezni@gmail.com>
2024-08-13 23:05:29 +0300

    Create MESSAGER.py

---
+++ MESSAGER.py
@@ -0,0 +1,36 @@
+from telethon.tl.functions.messages import SendMessageRequest
+from .. import loader
+
+@loader.tds
+class MessagerMod(loader.Module):
+    """Модуль для рассылки SMS в чат.
+           Commands: /sms.\n
+    ⚙️ By BENGAL & @pavlyxa_rezon"""
+
+    strings = {"name": "BGL-MESSAGER"}
+
+    async def send_message(self, chat_username, message_text):
+        done_message = f"<b>В чат {chat_username} успешно отправлена рассылка:</b>\n {message_text}"
+        fail_message = f"<b>Не удалось отправить в {chat_username} рассылку:</b>\n {message_text}"
+        
+        try:
+            chat_entity = await self._client.get_entity(chat_username)
+            await self._client(SendMessageRequest(peer=chat_entity, message=message_text))
+            await self.send_me_message(done_message)
+        except:
+            await self.send_me_message(fail_message)
+
+    async def send_me_message(self, text):
+        await self.client.send_message('me', text)
+
+    @loader.watcher()
+    async def watcher(self, message):
+        chat_id = -1002187569778
+        if message.chat_id == chat_id and message.text.startswith("/sms"):
+            args = message.text.split(" ", 2)
+            if len(args) < 3:
+                return
+            chat_username, message_text = args[1], args[2]
+            if not chat_username.startswith("@"):
+                return
+            await self.send_message(chat_username, message_text)
