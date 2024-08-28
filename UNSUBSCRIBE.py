e4dc27b15f01d6388251f1728ac6a6fa653ac133
BENGALX <pavlikrezni@gmail.com>
2024-08-29 00:03:07 +0300

    Update UNSUBSCRIBE.py

--- UNSUBSCRIBE.py
+++ UNSUBSCRIBE.py
@@ -13,44 +13,55 @@ class UNSUBMod(loader.Module):
     async def send_me_message(self, text):
         await self.client.send_message('me', text, link_preview=False)
 
+    async def unsubscribe_by_tag(self, target):
+        done_message = f"<b>Вы успешно отписались от</b> {target}"
+        user_message = f"<b>Вы успешно удалили чат с</b> {target}"
+        try:
+            await self.client(functions.channels.LeaveChannelRequest(target))
+            await self.send_me_message(done_message)
+        except:
+            await self.client.delete_dialog(target)
+            await self.send_me_message(user_message)
+
+    async def unsubscribe_by_link(self, target):
+        match = re.search(r't\.me/([a-zA-Z0-9_]+)', target)
+        done_message = f"<b>Вы успешно отписались от</b>\n{target}"
+        user_message = f"<b>Вы успешно удалили чат с</b>\n{target}"
+        if match:
+            username = match.group(1)
+            try:
+                await self.client(functions.channels.LeaveChannelRequest(username))
+                await self.send_me_message(done_message)
+            except:
+                await self.client.delete_dialog(username)
+                await self.send_me_message(user_message)
+        else:
+            await self.send_me_message("Не удалось извлечь имя пользователя.")
+
+    async def unsubscribe_by_id(self, target):
+        done_message = f"<b>Вы успешно отписались от ID:</b>{target}"
+        user_message = f"<b>Вы успешно удалили чат с ID:</b> {target}"
+        try:
+            channel_id = int(target)
+            await self.client(functions.channels.LeaveChannelRequest(channel_id))
+            await self.send_me_message(done_message)
+        except:
+            await self.client.delete_dialog(channel_id)
+            await self.send_me_message(user_message)
+
     @loader.watcher()
-    async def unsubscribe_channel(self, message):
+    async def watcher_group(self, message):
         if message.chat_id != -1002205010643:
             return
 
         if message.text.startswith("/uns"):
             target = message.text.split("/uns", 1)[1].strip()
-            
-            done_message = f"<b>Вы успешно отписались от</b> {target}"
-            user_message = f"<b>Вы успешно удалили чат с</b> {target}"
-            fail_message = f"<b>Цель удаления не распознана</b> {target}"
-
+            fail_message = f"<b>Цель удаления не распознана:</b> {target}"
             if target.startswith("@"):
-                try:
-                    await self.client(functions.channels.LeaveChannelRequest(target))
-                    await self.send_me_message(done_message)
-                except:
-                    await self.client.delete_dialog(target)
-                    await self.send_me_message(user_message)
+                await self.unsubscribe_by_tag(target)
             elif "t.me/" in target:
-                match = re.search(r't\.me/([a-zA-Z0-9_]+)', target)
-                if match:
-                    username = match.group(1)
-                    try:
-                        await self.client(functions.channels.LeaveChannelRequest(username))
-                        await self.send_me_message(done_message)
-                    except:
-                        await self.client.delete_dialog(username)
-                        await self.send_me_message(user_message)
-                else:
-                    await self.send_me_message(f"Не вышло извлечь имя пользователя.")
+                await self.unsubscribe_by_link(target)
             elif target.isdigit():
-                try:
-                    channel_id = int(target)
-                    await self.client(functions.channels.LeaveChannelRequest(channel_id))
-                    await self.send_me_message(done_message)
-                except:
-                    await self.client.delete_dialog(channel_id)
-                    await self.send_me_message(user_message)
+                await self.unsubscribe_by_id(target)
             else:
                 await self.send_me_message(fail_message)
