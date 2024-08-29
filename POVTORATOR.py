57e53ef675f16e3671454571977873b6519c6354
BENGALX <pavlikrezni@gmail.com>
2024-08-29 19:21:58 +0300

    Update POVTORATOR.py

--- POVTORATOR.py
+++ POVTORATOR.py
@@ -5,24 +5,33 @@ from .. import loader
 class PovtoratorMod(loader.Module):
     """Модуль для вывода сообщений.
            Команда: /comanda.\n
-    ⚙️ By BENGAL & @pavlyxa_rezon"""
+    ⚙️ By @pavlyxa_rezon"""
 
     strings = {"name": "BGL-POVTORATOR"}
 
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "custom_uid", -1002205010643, "CustomID",
+                validator=loader.validators.Integer(),
+            )
+        )
+
     async def send_message(self, chat_id, message_text):
         try:
-            chat_entity = await self._client.get_entity(chat_id)
-            await self._client(SendMessageRequest(peer=chat_entity, message=message_text))
+            chat_entity = await self.client.get_entity(chat_id)
+            await self.client(SendMessageRequest(peer=chat_entity, message=message_text))
         except:
             pass
 
+    async def handle_command(self, text):
+        message_text = text.split("/comanda", 1)[1].strip()
+        chat_id = self.config["custom_uid"]
+        await self.send_message(chat_id, message_text)
+
     @loader.watcher()
     async def watcher(self, message):
-        if message.chat_id != -1002205010643:
+        if message.chat_id != self.config["custom_uid"]:
             return
-        try:
-            if message.message.startswith("/comanda"):
-                message_text = message.text.split("/comanda", 1)[1].strip()
-                await self.send_message(-1002205010643, message_text)
-        except:
-            pass
+        if message.text.startswith("/comanda"):
+            await self.handle_command(message.text)
