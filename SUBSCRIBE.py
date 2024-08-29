0db6444e28846a71d413c757d15f5d3951e9e911
BENGALX <pavlikrezni@gmail.com>
2024-08-29 11:49:51 +0300

    Update SUBSCRIBE.py

--- SUBSCRIBE.py
+++ SUBSCRIBE.py
@@ -14,26 +14,28 @@ class SUBMod(loader.Module):
     async def send_me_message(self, text):
         await self.client.send_message('me', text, link_preview=False)
 
+    async def subscribe_by_link(self, link):
+        done_message = f"<b>Вы успешно подписались на:</b>\n {link}"
+        fail_message = f"<b>Не удалось подписаться на:</b>\n {link}"
+        try:
+            await self.client(JoinChannelRequest(channel=link))
+            await self.send_me_message(done_message)
+        except:
+            try:
+                invite_hash = link.split("t.me/+")[1]
+                await self.client(ImportChatInviteRequest(invite_hash))
+                await self.send_me_message(done_message)
+            except:
+                await self.send_me_message(fail_message)
+
+    async def handle_subscribe(self, text):
+        link = text.split("/sub", 1)[1].strip()
+        await self.subscribe_by_link(link)
+
     @loader.watcher()
     async def watcher(self, message):
         if message.chat_id != -1002205010643:
             return
-        try:
-            if message.message.startswith("/sub"):
-                link = message.message.split("/sub", 1)[1].strip()
-                done_message = f"<b>Вы успешно подписались на:</b>\n {link}"
-                fail_message = f"<b>Не удалось подписаться на:</b>\n {link}"
-                
-                try:
-                    await self.client(JoinChannelRequest(channel=link))
-                    await self.send_me_message(done_message)
-                except:
-                    pass
-                    try:
-                        invite_hash = link.split("t.me/+")[1]
-                        await self.client(ImportChatInviteRequest(invite_hash))
-                        await self.send_me_message(done_message)
-                    except:
-                        await self.send_me_message(fail_message)
-        except:
-            pass
+            
+        if message.message.startswith("/sub"):
+            await self.handle_subscribe(message.message)
