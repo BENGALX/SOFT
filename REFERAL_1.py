ddb122d7a607378393a8735d631944eaaacbd3b8
BENGALX <pavlikrezni@gmail.com>
2024-08-30 14:57:37 +0300

    Update and rename REFERAL.py to REFERAL_1.py

--- REFERAL_1.py
+++ REFERAL_1.py
@@ -34,30 +34,28 @@ class ReferalMod(loader.Module):
     async def send_me_message(self, text):
         await self.client.send_message('me', text, link_preview=False)
 
+    async def handle_referral(self, text):
+        linka = text.split("/ref", 1)[1].strip()
+        done_message = f"<b>Вы успешно стартанули рефку:</b> \n {linka}"
+        fail_message = f"<b>Введена неправильная ссылка:</b> \n {linka}"
+        if "BestRandom_bot" in text:
+            await self.start_bestrandom_bot(text)
+            await self.send_me_message(done_message)
+        elif "TheFastes_Bot" in text:
+            await self.start_fastes_bot(text)
+            await self.send_me_message(done_message)
+        elif "TheFastesRuBot" in text:
+            await self.start_fastesru_bot(text)
+            await self.send_me_message(done_message)
+        elif "GiveawayLuckyBot" in text:
+            await self.start_givelucky_bot(text)
+            await self.send_me_message(done_message)
+        else:
+            await self.send_me_message(fail_message)
+
     @loader.watcher(only_channels=True)
     async def watcher_bot(self, message: Message):
         if message.chat_id != -1002187149618:
             return
         if message.text.startswith("/ref"):
-            linka = message.text.split("/ref", 1)[1].strip()
-            done_message = f"<b>Вы успешно стартанули рефку:</b> \n {linka}"
-            fail_message = f"<b>Введена неправильная ссылка:</b> \n {linka}"
-            
-            if "BestRandom_bot" in message.text:
-                await self.start_bestrandom_bot(message.text)
-                await self.send_me_message(done_message)
-
-            elif "TheFastes_Bot" in message.text:
-                await self.start_fastes_bot(message.text)
-                await self.send_me_message(done_message)
-
-            elif "TheFastesRuBot" in message.text:
-                await self.start_fastesru_bot(message.text)
-                await self.send_me_message(done_message)
-
-            elif "GiveawayLuckyBot" in message.text:
-                await self.start_givelucky_bot(message.text)
-                await self.send_me_message(done_message)
-
-            else:
-                await self.send_me_message(fail_message)
+            await self.handle_referral(message.text)
