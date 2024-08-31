6e23aef09a79558dd0d19fbc4247d53948f55b69
BENGALX <pavlikrezni@gmail.com>
2024-08-31 16:52:28 +0300

    Update MESSAGER.py

--- MESSAGER.py
+++ MESSAGER.py
@@ -3,18 +3,26 @@ from .. import loader
 
 @loader.tds
 class MessagerMod(loader.Module):
-    """Модуль для рассылки SMS в чат.
+    """Модуль для рассылки SMS в чаты.
            Commands: /sms.\n
     ⚙️ By @pavlyxa_rezon"""
 
     strings = {"name": "BGL-MESSAGER"}
 
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "custom_uid", -1002205010643, "CustomID",
+                validator=loader.validators.Integer(),
+            )
+        )
+  
     async def send_message(self, chat_username, message_text):
         done_message = f"<b>В чат {chat_username} успешно отправлена рассылка:</b>\n {message_text}"
         fail_message = f"<b>Не удалось отправить в {chat_username} рассылку:</b>\n {message_text}"
         try:
-            chat_entity = await self._client.get_entity(chat_username)
-            await self._client(SendMessageRequest(peer=chat_entity, message=message_text))
+            chat_entity = await self.client.get_entity(chat_username)
+            await self.client(SendMessageRequest(peer=chat_entity, message=message_text))
             await self.send_me_message(done_message)
         except:
             await self.send_me_message(fail_message)
@@ -33,7 +41,7 @@ class MessagerMod(loader.Module):
 
     @loader.watcher()
     async def watcher_group(self, message):
-        if message.chat_id != -1002205010643:
+        if message.chat_id != self.config["custom_uid"]:
             return
         
         if message.text.startswith("/sms"):
