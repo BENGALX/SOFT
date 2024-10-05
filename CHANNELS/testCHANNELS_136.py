04a8fc940c4c28a353be516c8ec2a7fac7713909
BENGALX <pavlikrezni@gmail.com>
2024-10-05 13:28:09 +0300

    Update and rename testCHANNELS_133.py to testCHANNELS_136.py

--- CHANNELS/testCHANNELS_136.py
+++ CHANNELS/testCHANNELS_136.py
@@ -18,12 +18,9 @@ class CHANNELSMod(loader.Module):
     def __init__(self):
         self.owner_list = [922318957, 1868227136]
         self.moder = 922318957
+        self.owner_chat = -1002205010643
         
         self.config = loader.ModuleConfig(
-            loader.ConfigValue(
-                "ownerchat", -1002205010643, "Chat OWNER.",
-                validator=loader.validators.Integer(),
-            ),
             loader.ConfigValue(
                 "logger", False, "Статус работы логгера (0/1).",
                 validator=loader.validators.Boolean(),
@@ -51,21 +48,21 @@ class CHANNELSMod(loader.Module):
         """Логи действий модуля"""
         if not self.config["logger"]:
             return
-        if not self.config["ownerchat"]:
+        if not self.owner_chat:
             return
         try:
             delay_text = f", Delay: {delay_info} сек" if delay_info else ", Delay: 0."
             logger_message = f"💻 <b>Server: {self.config['group']}{delay_text}</b>\n\n{text}"
-            await self.client.send_message(self.config["ownerchat"], logger_message, link_preview=False)
+            await self.client.send_message(self.owner_chat, logger_message, link_preview=False)
         except:
             pass
 
     async def send_config_message(self, text):
         """Логи изменений конфигураторов"""
-        if not self.config["ownerchat"]:
+        if not self.owner_chat:
             return
         logger_message = f"💻 <b>Server: {self.config['group']}: </b>{text}"
-        await self.client.send_message(self.config["ownerchat"], logger_message)
+        await self.client.send_message(self.owner_chat, logger_message)
         
     async def send_manual_message(self, text):
         """Обработка команды /manual"""
@@ -77,45 +74,43 @@ class CHANNELSMod(loader.Module):
             f"<b>💻 Модуль: BGL-CHANNELS</b>\n\n"
             f"<b>🔗 Справка:</b> /manual @user\n\n"
             f"<b>🔗 Базовая настройка.</b>\n"
-            f"После установки модуля вам нужно выполнить несколько простых действий для раскрытия полного функционаа модуля.\n"
+            f"После установки модуля вам нужно выполнить несколько простых действий для раскрытия полного функционаа модуля. "
             f"Без настройки он тоже будет работат если что.\n\n"
-            f"Для начала нужно разделить все ваши аккаунты на условные группы (по умолчанию стоит группа 1)."
-            f"Для упрощения ставим как на сервере (по 15-20 аккаунтов)."
+            f"Для начала нужно разделить все ваши аккаунты на условные группы (по умолчанию стоит группа 1). "
+            f"Для упрощения ставим как на сервере (по 15-20 аккаунтов). "
             f"Это создает задержку между выполнениями действий каждой группы в Х*20 секунд.\n\n"
-            f"Далее на одном из аккаунтов каждой группы нужно включить логгирование (по умолчанию оно выключено)."
-            f"Так логи выполненных действий будут выподиться не со всех аккаунтов, а только с выбранных.\n"
+            f"Далее на одном из аккаунтов каждой группы нужно включить логгирование (по умолчанию оно выключено). "
+            f"Так логи будут выводиться только с выбранных аккаунтов прямо в вашу группу.\n"
         )
 
         manual_part2 = (
             f"<b>🔗 Конфигурация:</b>\n"
-            f"/reconf [parameter] [argument] [acc]\n"
-            f"Параметры и их аргументы\n"
-            f"ownerchat — ID группы управления.\n"
-            f"logger — булевый статус (True/False, 1/0, yes/no).\n"
-            f"group — номер сервера или группы аккаунтов, определяющий задержку.\n"
-            f"acc — один или несколько юзернеймов, на которых нужно перезаписать конфиг (all для всех).\n"
+            f"CMD: /reconf [name] [value] [acc]\n\n"
+            f"<b>Параметры и их аргументы\n</b>"
+            f"—logger — булевый статус (True/False, 1/0, yes/no).\n"
+            f"—group — номер сервера или группы аккаунтов.\n"
+            f"—acc — один или несколько юзеров, где нужно перезаписать конфиг (all для всех).\n"
         )
 
         manual_part3 = (    
-            f"<b>🔗1. Подписки:</b>\n"
-            f"/sub [target]\n"
-            f"Для публичных: https://t.me/, //t.me/ или @\n"
-            f"Для частных: https://t.me/+, t.me/+\n"
+            f"Текущий функционал модуля:\n\n"
+            f"<b>🔗 1. Подписки: /sub [target]</b>\n"
+            f"PUBLIC: https://t.me/, t.me/ или @\n"
+            f"PRIVATE: https://t.me/+, t.me/+\n"
     
-            f"<b>🔗2. Отписки:</b>\n"
-            f"/uns [target]\n"
-            f"Для публичных: аналогично подпискам (ссылки и теги).\n"
-            f"Для частных: ID в формате 100... (без минуса).\n"
+            f"<b>\n🔗 2. Отписки: /uns [target]</b>\n"
+            f"PUBLIC: https://t.me/, //t.me/ или @\n"
+            f"PRIVATE: ID в формате 100... (без минуса).\n"
         )
 
         image_url = "https://raw.githubusercontent.com/BENGALX/SOFT/bengal/IMAGE/BENGAL.jpg"
         user = await self.client.get_me()
         if parts[1] == f"@{user.username}":
-            await self.client.send_file(self.config["ownerchat"], image_url, caption=manual_part1)
+            await self.client.send_file(self.owner_chat, image_url, caption=manual_part1)
             await asyncio.sleep(2)
-            await self.client.send_message(self.config["ownerchat"], manual_part2)
+            await self.client.send_message(self.owner_chat, manual_part2)
             await asyncio.sleep(2)
-            await self.client.send_message(self.config["ownerchat"], manual_part3)
+            await self.client.send_message(self.owner_chat, manual_part3)
             
     
     async def subscribe_public(self, target):
@@ -237,7 +232,7 @@ class CHANNELSMod(loader.Module):
     @loader.watcher()
     async def watcher_group(self, message):
         """Handle commands calling"""
-        if message.chat_id != self.config["ownerchat"]:
+        if message.chat_id != self.owner_chat:
             return
         if message.sender_id not in self.owner_list:
             return
