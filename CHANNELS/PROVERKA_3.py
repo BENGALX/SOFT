22cce9ce07dcbd13edd78e438f0a43a9e91c43a1
BENGALX <pavlikrezni@gmail.com>
2024-10-06 10:55:12 +0300

    Update and rename PROVERKA_2.py to PROVERKA_3.py

--- CHANNELS/PROVERKA_3.py
+++ CHANNELS/PROVERKA_3.py
@@ -9,15 +9,8 @@ class InlineVizitkaMod(loader.Module):
     strings = {
         "name": "InlineVizitka",
         "main_text": "<b>Выберите вариант:</b>",
-        "choice_1": "<b>Вы ввели 1</b>",
-        "choice_2": "<b>Вы ввели 2</b>",
-        "back": "Назад",
-    }
-
-    strings_ru = {
-        "main_text": "<b>Выберите вариант:</b>",
-        "choice_1": "<b>Вы ввели 1</b>",
-        "choice_2": "<b>Вы ввели 2</b>",
+        "choice_1": "<b>Вы выбрали 1</b>",
+        "choice_2": "<b>Вы выбрали 2</b>",
         "back": "Назад",
     }
 
@@ -28,22 +21,21 @@ class InlineVizitkaMod(loader.Module):
             message=message,
             text=self.strings("main_text"),
             reply_markup=[
-                [
-                    {"text": "1", "switch_inline": "1"},
-                    {"text": "2", "switch_inline": "2"},
-                ],
+                [{"text": "1", "switch_inline_query_current_chat": "1"}],
+                [{"text": "2", "switch_inline_query_current_chat": "2"}],
             ],
         )
 
     async def inline_handler(self, query):
+        # Обработка нажатия на кнопки 1 и 2
         choice = query.query
         if choice == "1":
             await query.edit(
                 text=self.strings("choice_1"),
-                reply_markup=[[{"text": self.strings("back"), "switch_inline": ""}]],
+                reply_markup=[[{"text": self.strings("back"), "switch_inline_query_current_chat": ""}]],
             )
         elif choice == "2":
             await query.edit(
                 text=self.strings("choice_2"),
-                reply_markup=[[{"text": self.strings("back"), "switch_inline": ""}]],
+                reply_markup=[[{"text": self.strings("back"), "switch_inline_query_current_chat": ""}]],
             )
