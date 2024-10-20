0a599fcaa11ed1349df077a5006a13f57ad411f9
BENGALX <pavlikrezni@gmail.com>
2024-10-20 19:23:41 +0300

    Update BENGALSOFT_v665.py

--- MAIN/BENGALSOFT_v665.py
+++ MAIN/BENGALSOFT_v665.py
@@ -9,6 +9,7 @@ from telethon.tl.functions.channels import JoinChannelRequest, LeaveChannelReque
 from telethon.tl.functions.messages import ImportChatInviteRequest, StartBotRequest, GetMessagesViewsRequest
 
 from telethon.errors.rpcerrorlist import UserNotParticipantError, ChannelInvalidError, PeerIdInvalidError
+from telethon.errors import UsernameNotOccupiedError
 
 @loader.tds
 class BENGALSOFTMod(loader.Module):
@@ -220,7 +221,7 @@ class BENGALSOFTMod(loader.Module):
                     await self.send_done_message(f"<b>♻️ UNSUB by <a href='{target}'>PUBL LINK</a></b>", delay_info=(mult, delay_s))
                 except UserNotParticipantError:
                     await self.send_done_message(f"<b>⚠️ UNSUB: NONE IN <a href='{target}'>PUBL LINK</a></b>", delay_info=(mult, delay_s))
-                except (ChannelInvalidError, PeerIdInvalidError):
+                except (ChannelInvalidError, PeerIdInvalidError, UsernameNotOccupiedError):
                     await self.send_done_message(f"<b>🚫 UNSUB: INVALID LINK.</b>", delay_info=(mult, delay_s))
                 except:
                     await self.client.delete_dialog(username)
