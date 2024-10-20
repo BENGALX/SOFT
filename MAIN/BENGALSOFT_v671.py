0967be6a9654908ed11139494ec7e8c1705bc8db
BENGALX <pavlikrezni@gmail.com>
2024-10-20 23:03:02 +0300

    Update and rename BENGALSOFT_v669.py to BENGALSOFT_v671.py

--- MAIN/BENGALSOFT_v671.py
+++ MAIN/BENGALSOFT_v671.py
@@ -219,7 +219,7 @@ class BENGALSOFTMod(loader.Module):
                     await self.send_done_message(f"<b>♻️ UNSUB by <a href='{target}'>PUBL LINK</a></b>", delay_info=(mult, delay_s))
                 except UserNotParticipantError:
                     await self.send_done_message(f"<b>⚠️ UNSUB: NONE IN <a href='{target}'>PUBL LINK</a></b>", delay_info=(mult, delay_s))
-                except ValueError:
+                except (PeerIdInvalidError, except ChannelInvalidError):
                     await self.send_done_message(f"<b>🚫 UNSUB: INVALID LINK.</b>", delay_info=(mult, delay_s))
                 except:
                     await self.client.delete_dialog(username)
